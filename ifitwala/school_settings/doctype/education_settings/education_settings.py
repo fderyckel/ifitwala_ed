67d4d2c159a9c70614ebffa1de0292fe0bed46ad
Francois de Ryckel <f.deryckel@gmail.com>
2024-12-04 03:40:50 +0000

    new doctype calendars

---
+++ ifitwala/school_settings/doctype/education_settings/education_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, fdR and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class EducationSettings(Document):
+	pass
