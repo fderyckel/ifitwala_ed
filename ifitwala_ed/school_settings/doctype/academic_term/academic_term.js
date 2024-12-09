fd047c644a2961114f63fcd76f6fa6702fd4378f
Francois de Ryckel <f.deryckel@gmail.com>
2024-12-09 20:00:51 +0700

    change list view to calendar view

--- ifitwala_ed/school_settings/doctype/academic_term/academic_term.js
+++ ifitwala_ed/school_settings/doctype/academic_term/academic_term.js
@@ -1,14 +1,18 @@
 // Copyright (c) 2020, ifitwala and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Academic Term', {
-	refresh: function(frm) {
-	},
+frappe.ui.form.on("Academic Term", {
+  refresh: function (frm) {},
 
-	term_start_date: function(frm) {
-		if(frm.doc.term_start_date && !frm.doc.term_end_date) {
-			var a_year_from_start = frappe.datetime.add_months(frm.doc.term_start_date, 4);
-			frm.set_value('term_end_date', frappe.datetime.add_days(a_year_from_start, - 1));
-		}
-	}
+  term_start_date: function (frm) {
+    if (frm.doc.term_start_date && !frm.doc.term_end_date) {
+      var a_year_from_start = frappe.datetime.add_months(
+        frm.doc.term_start_date
+      );
+      frm.set_value(
+        "term_end_date",
+        frappe.datetime.add_days(a_year_from_start, -1)
+      );
+    }
+  },
 });
---
+++ ifitwala_ed/school_settings/doctype/school_event/school_event_list.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2024, François de Ryckel and contributors
+// For license information, please see license.txt
+
+frappe.listview_settings["Event"].onload = function (listview) {
+  window.location.href = "/app/school_event/view/calendar/default";
+};
