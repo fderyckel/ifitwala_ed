3b361beb772e0ea7924b05f5f3b8932ea595c59c
Francois de Ryckel <f.deryckel@gmail.com>
2024-12-15 22:56:30 +0700

    reoder some doctypes in different modules and deleted duplicated doctypes

--- ifitwala_ed/curriculum/doctype/course/course.js
+++ ifitwala_ed/curriculum/doctype/course/course.js
@@ -1,8 +1,105 @@
-// Copyright (c) 2024, fdR and contributors
+// Copyright (c) 2024, François de Ryckel and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Course", {
-// 	refresh(frm) {
+frappe.ui.form.on("Course", {
+  setup: function (frm) {
+    frm.add_fetch("team", "school", "school");
+  },
 
-// 	},
-// });
+  refresh: function (frm) {
+    if (!cur_frm.doc.__islocal) {
+      frm.add_custom_button(
+        __("Add to Programs"),
+        function () {
+          frm.trigger("add_course_to_programs");
+        },
+        __("Action")
+      );
+    }
+
+    // to only suggest grade scale that are submitted (not cancel nor draft)
+    frm.set_query("default_grade_scale", function () {
+      return {
+        filters: {
+          docstatus: 1,
+        },
+      };
+    });
+  },
+
+  add_course_to_programs: function (frm) {
+    get_programs_without_course(frm.doc.name).then((r) => {
+      if (r.message.length) {
+        frappe.prompt(
+          [
+            {
+              fieldname: "programs",
+              label: __("Programs"),
+              fieldtype: "MultiSelectPills",
+              get_data: function () {
+                return r.message;
+              },
+            },
+            {
+              fieldtype: "Check",
+              label: __("Is Mandatory"),
+              fieldname: "mandatory",
+            },
+          ],
+          function (data) {
+            frappe.call({
+              method:
+                "ifitwala_ed.curriculum.doctype.course.course.add_course_to_programs",
+              args: {
+                course: frm.doc.name,
+                programs: data.programs,
+                mandatory: data.mandatory,
+              },
+              callback: function (r) {
+                if (!r.exc) {
+                  frm.reload_doc();
+                }
+              },
+              freeze: true,
+              freeze_message: __("...Adding Course to Programs"),
+            });
+          },
+          __("Add Course to Programs"),
+          __("Add")
+        );
+      } else {
+        frappe.msgprint(
+          __("This course is already added to the existing programs")
+        );
+      }
+    });
+  },
+});
+
+// to filter out assessment criteria that have already been picked out in the course.
+frappe.ui.form.on("Course Assessment Criteria", {
+  criteria_add: function (frm) {
+    frm.fields_dict["assessment_criteria"].grid.get_field(
+      "assessment_criteria"
+    ).get_query = function (doc) {
+      var criteria_list = [];
+      if (!doc.__islocal) criteria_list.push(doc.name);
+      $.each(doc.assessment_criteria, function (idx, val) {
+        if (val.assessment_criteria)
+          criteria_list.push(val.assessment_criteria);
+      });
+      return {
+        filters: [["Assessment Criteria", "name", "not in", criteria_list]],
+      };
+    };
+  },
+});
+
+let get_programs_without_course = function (course) {
+  return frappe.call({
+    type: "GET",
+    method:
+      "ifitwala_ed.curriculum.doctype.course.course.get_programs_without_course",
+    args: { course: course },
+  });
+};
--- ifitwala_ed/curriculum/doctype/course/course.py
+++ ifitwala_ed/curriculum/doctype/course/course.py
@@ -1,9 +1,62 @@
-# Copyright (c) 2024, fdR and contributors
+# Copyright (c) 2024, François de Ryckel and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import json
+from frappe import _
 from frappe.model.document import Document
 
-
 class Course(Document):
-	pass
+	def validate(self):
+		self.validate_sum_weighting
+		ass_criteria = []
+		for d in self.assessment_criteria:
+			if d.assessment_criteria in ass_criteria:
+				frappe.throw(_("Assessment Criteria {0} appears more than once. Please remove duplicate entries.").format(d.assessment_criteria))
+			else:
+				ass_criteria.append(d.assessment_criteria)
+
+
+	def validate_sum_weighting(self):
+		if self.assessment_criteria:
+			total_weight = 0
+			for criteria in assessment_criteria:
+				total_weight += assessment_criteria.criteria_weighting or 0
+			if total_weight != 100:
+				frappe.throw(_("The sum of the Criteria Weighting should be 100%.  Please adjust and try to save again."))
+
+	def get_learning_units(self):
+		lu_data = []
+		for unit in self.units:
+			unit_doc = frappe.get_doc("Learning Unit", unit.learning_unit)
+			if unit_doc.unit_name:
+				lu_data.append(unit_doc)
+		#lu_data = lu_data.sort(key=lambda x: x.start_date)
+		return lu_data
+
+
+@frappe.whitelist()
+def add_course_to_programs(course, programs, mandatory=False):
+	programs = json.loads(programs)
+	for entry in programs:
+		program = frappe.get_doc('Program', entry)
+		program.append('courses', {
+			'course': course,
+			'course_name': course,
+			'mandatory': mandatory
+		})
+		program.flags.ignore_mandatory = True
+		program.save()
+	frappe.db.commit()
+	frappe.msgprint(_('Course {0} has been added to all the selected programs successfully.').format(frappe.bold(course)),
+		title=_('Programs updated'), indicator='green')
+
+@frappe.whitelist()
+def get_programs_without_course(course):
+	data = []
+	for entry in frappe.db.get_all('Program'):
+		program = frappe.get_doc('Program', entry.name)
+		courses = [c.course for c in program.courses]
+		if not courses or course not in courses:
+			data.append(program.name)
+	return data
