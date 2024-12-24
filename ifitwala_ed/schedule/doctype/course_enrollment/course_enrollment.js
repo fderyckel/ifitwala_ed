d38247af237096403a43f6f44e1ece3ebebc25d3
fdr <f.deryckel@gmail.com>
2024-12-24 11:06:07 +0000

    doctypes related to schedule

---
+++ ifitwala_ed/schedule/doctype/course_enrollment/course_enrollment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, François de Ryckel and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Course Enrollment", {
+// 	refresh(frm) {
+
+// 	},
+// });
