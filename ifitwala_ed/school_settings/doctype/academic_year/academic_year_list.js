c1c3338cd61003909df45154a6d76bc2dc592bdf
Francois de Ryckel <f.deryckel@gmail.com>
2024-12-27 11:40:46 +0700

    back to default

--- ifitwala_ed/school_settings/doctype/academic_year/academic_year_list.js
+++ ifitwala_ed/school_settings/doctype/academic_year/academic_year_list.js
@@ -3,24 +3,4 @@
 
 frappe.listview_settings["Academic Year"] = {
   hide_name_column: true,
-  get_columns: function () {
-    return [
-      {
-        field: "ID",
-        width: "150px",
-      },
-      {
-        field: "academic_year_name",
-        width: "150px",
-      },
-      {
-        field: "school",
-        width: "200px",
-      },
-      {
-        field: "year_start_date",
-        width: "150px",
-      },
-    ];
-  },
 };
