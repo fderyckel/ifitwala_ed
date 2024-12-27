7af3eadaf5524fdc8ec88ca2737e88a5590f1b13
Francois de Ryckel <f.deryckel@gmail.com>
2024-12-27 11:14:43 +0700

    resizing columns in academic year List view.

--- ifitwala_ed/school_settings/doctype/academic_year/academic_year_list.js
+++ ifitwala_ed/school_settings/doctype/academic_year/academic_year_list.js
@@ -1,6 +1,26 @@
-// Copyright (c) 2020, François de Ryckel and contributors
+// Copyright (c) 2024, François de Ryckel and contributors
 // For license information, please see license.txt
 
 frappe.listview_settings["Academic Year"] = {
   hide_name_column: true,
+  get_columns: function () {
+    return [
+      {
+        field: "ID",
+        width: "150px",
+      },
+      {
+        field: "academic_year_name",
+        width: "150px",
+      },
+      {
+        field: "school",
+        width: "200px",
+      },
+      {
+        field: "year_start_date",
+        width: "150px",
+      },
+    ];
+  },
 };
