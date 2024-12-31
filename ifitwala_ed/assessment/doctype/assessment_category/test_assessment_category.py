cbda4ab7aa00704614f37482a7f3d9a6a65edfbc
fdr <f.deryckel@gmail.com>
2024-12-31 03:29:31 +0000

    changes in assessment criteria

---
+++ ifitwala_ed/assessment/doctype/assessment_category/test_assessment_category.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, François de Ryckel and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAssessmentCategory(FrappeTestCase):
+	pass
