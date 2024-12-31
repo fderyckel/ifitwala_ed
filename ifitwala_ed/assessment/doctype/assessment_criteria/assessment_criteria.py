ee5605264d33aeb6e1f363e9e082977ab15511b9
Francois de Ryckel <f.deryckel@gmail.com>
2024-12-31 11:05:04 +0700

    validation rule for assessment criteria

--- ifitwala_ed/assessment/doctype/assessment_criteria/assessment_criteria.py
+++ ifitwala_ed/assessment/doctype/assessment_criteria/assessment_criteria.py
@@ -1,9 +1,28 @@
-# Copyright (c) 2024, fdR and contributors
+# Copyright (c) 2024, François de Ryckek and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
+
+STD_CRITERIA = ["total", "total score", "total grade", "maximum score", "score", "grade"]
+
+class AssessmentCriteria(Document):  
+	
+	def validate(self):
+		if self.assessment_criteria.lower() in STD_CRITERIA:
+			frappe.throw(_("These are reserved Key words. Please rename the criteria"))
+		
+		if self.course_group or self.abbr: 
+			title_parts = ["Crit"]
+			if self.course_group:
+				title_parts.append(self.course_group)
+			if self.abbr:
+				title_parts.append(self.abbr)
+			self.title = "-".join(title_parts)
+		else:
+			words = self.assement_criteria.split()
+			abbreviation = "".join(word[:2].capitalize() for word in words)
+			self.title = f"CRIT-{abbreviation}"
 
 
-class AssessmentCriteria(Document):
-	pass
