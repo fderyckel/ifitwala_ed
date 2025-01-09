83de31ad44cfd7df1c67772302be72b484a92315
Francois de Ryckel <f.deryckel@gmail.com>
2025-01-09 20:39:14 +0700

    refactor few validation rules in program enrollment

--- ifitwala_ed/schedule/doctype/program_enrollment/program_enrollment.py
+++ ifitwala_ed/schedule/doctype/program_enrollment/program_enrollment.py
@@ -15,7 +15,21 @@ class ProgramEnrollment(Document):
 		if not self.student_name:
 			self.student_name = frappe.db.get_value("Student", self.student, "student_full_name")
 		if not self.courses:
-			self.extend("courses", self.get_courses())
+			self.extend("courses", self.get_courses()) 
+
+		if self.academic_year: 
+			year_dates = frappe.get_doc("Academic Year", self.academic_year)
+			if self.enrollment_date: 
+				if getdate(self.enrollment_date) < getdate(year_dates.year_start_date): 
+					frappe.throw(_("The enrollment date for this program is before the start of the academic year {0}. The academic year starts on {1}.  Please revise the date.").format(
+						get_link_to_form("Academic Year", self.academic_year), 
+						year_dates.year_start_date
+					))
+				if getdate(self.enrollment_date) > getdate(year_dates.year_end_date): 
+					frappe.throw(_("The enrollment date for this program is after the start of the academic year {0}. The academic year ends on {1}.  Please revise the date.").format(
+						get_link_to_form("Academic Year", self.academic_year), 
+						year_dates.year_end_date
+					))
 
 		if self.term:
 			term_dates = frappe.get_doc("Term", self.term)
@@ -31,18 +45,19 @@ class ProgramEnrollment(Document):
 		self.create_course_enrollment()
 
 	# you cannot enrolled twice for a same program, same year, same term.
-	def validate_duplication(self):
-		enrollment = frappe.get_all("Program Enrollment", fields = ["name", "student_name"],
-				filters = {
-					"student": self.student,
-					"academic_year": self.academic_year,
-					"term": self.term,
-					"program": self.program,
-					"docstatus": ("<", 2),
-					"name": ("!=", self.name)
-				})
-		if enrollment:
-			frappe.throw(_("This student {0} is already enrolled {1} in this program for this term.").format(enrollment[0].student_name, get_link_to_form("Program Enrollment", enrollment[0].name)))
+	def validate_duplication(self): 
+		existing_enrollment_name = frappe.db.exists("Program Enrollment", { 
+			"student": self.student, 
+			"academic_year": self.academic_year, 
+			"term": self.term, 
+			"program": self.program, 
+			"docstatus": ("<", 2), 
+			"name": ("!=", self.name)
+		})
+		if existing_enrollment_name: 
+			student_name = frappe.db.get_value("student", self.student, "student_name")
+			link_to_existing_enrollment = get_link_to_form("Program Enrollment", existing_enrollment_name)
+			frappe.throw(_("Student {0} is already enrolled in this program for this term. See existing enrollment {1}").format(student_name, link_to_existing_enrollment))
 
 	# If a student is in a program and that program has required courses (non elective), then these courses are loaded automatically.
 	@frappe.whitelist()
@@ -71,8 +86,10 @@ class ProgramEnrollment(Document):
 
 	# This will update the joining date on the student doctype in function of the joining date of the program.
 	def update_student_joining_date(self):
-		date = frappe.db.sql("""select min(enrollment_date) from `tabProgram Enrollment` where student= %s""", self.student)
-		frappe.db.set_value("Student", self.student, "student_joining_date", date)
+		date_result = frappe.db.sql("""select min(enrollment_date) from `tabProgram Enrollment` where student= %s""", self.student)
+		if date_result and date_result[0] and date_result[0][0]:
+			date = date_result[0][0] 
+			frappe.db.set_value("Student", self.student, "student_joining_date", date)
 
 	def create_course_enrollment(self):
 		program = frappe.get_doc("Program", self.program)
@@ -92,9 +109,6 @@ class ProgramEnrollment(Document):
 @frappe.whitelist()
 @frappe.validate_and_sanitize_search_inputs
 def get_program_courses(doctype, txt, searchfield, start, page_len, filters):
-	#if not filters.get('program'):
-	#	frappe.msgprint(_("Please select a Program first."))
-	#	return []
 
 	return frappe.db.sql("""select course, course_name from `tabProgram Course`
 		where  parent = %(program)s and course like %(txt)s {match_cond}
