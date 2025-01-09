7879df1437a03841981300ce9fb51053a19fb906
Francois de Ryckel <f.deryckel@gmail.com>
2025-01-09 14:18:21 +0700

    not managing to use CSS with list view.. still totally buggy on frappe side

--- ifitwala_ed/students/doctype/student/student_list.js
+++ ifitwala_ed/students/doctype/student/student_list.js
@@ -2,17 +2,18 @@
 // For license information, please see license.txt
 
 frappe.listview_settings['Student'] = {
-    hide_name_column: true, 
-    add_fields: ["student_gender"], 
-    get_indicator: function (doc) {
-        if (doc.student_gender === "Female") {
-            console.log("Applying female-indicator to", doc.name); // Debugging log
-            return [__("Female"), "female-indicator", "student_gender,=,Female"]; // Light pink
-        } else if (doc.student_gender === "Male") {
-            console.log("Applying male-indicator to", doc.name); // Debugging log
-            return [__("Male"), "male-indicator", "student_gender,=,Male"]; // Light blue
-        } else {
-            return [__("Other"), "other-indicator", "student_gender,=,Other"]; // Light brown (tan)
-        }
+    hide_name_column: true, // Keep this if you want to hide the name column
+
+    get_indicator: function(doc) {
+
+        const gender_colors = {
+            "Female": "pink",
+            "Male": "light-blue",
+            "Other": "green"
+        };
+
+        let indicator_color = gender_colors[doc.student_gender] || "gray"; // Default to gray if not found
+
+        return [__(doc.student_gender), indicator_color,'student_gender,=,${doc.student_gender}'];
     }
 };
