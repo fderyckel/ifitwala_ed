406e34f26fe657d9f8ed8ed8017882541531df62
Francois de Ryckel <f.deryckel@gmail.com>
2025-01-09 14:06:25 +0700

    try again

--- ifitwala_ed/students/doctype/student/student_list.js
+++ ifitwala_ed/students/doctype/student/student_list.js
@@ -3,14 +3,16 @@
 
 frappe.listview_settings['Student'] = {
     hide_name_column: true, 
-    add_fields: ["student_gender", "docstatus"], 
+    add_fields: ["student_gender"], 
     get_indicator: function (doc) {
-        if (doc.student_gender === "Female") { 
-            return [__("Female"), "red", "student_gender,=,Female"]; // Light pink
+        if (doc.student_gender === "Female") {
+            console.log("Applying female-indicator to", doc.name); // Debugging log
+            return [__("Female"), "female-indicator", "student_gender,=,Female"]; // Light pink
         } else if (doc.student_gender === "Male") {
-            return [__("Male"), "blue", "student_gender,=,Male"]; // Light blue
+            console.log("Applying male-indicator to", doc.name); // Debugging log
+            return [__("Male"), "male-indicator", "student_gender,=,Male"]; // Light blue
         } else {
-            return [__("Other"), "green", "student_gender,=,Other"]; // Light brown (tan)
+            return [__("Other"), "other-indicator", "student_gender,=,Other"]; // Light brown (tan)
         }
     }
 };
