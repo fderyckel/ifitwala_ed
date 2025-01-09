c006dc520a91540aca61236405f889e01bda8086
Francois de Ryckel <f.deryckel@gmail.com>
2025-01-09 11:26:18 +0700

    adding debugging statements

--- ifitwala_ed/students/doctype/student/student_list.js
+++ ifitwala_ed/students/doctype/student/student_list.js
@@ -3,12 +3,15 @@
 
 frappe.listview_settings['Student'] = {
   get_indicator: function (doc) {
-      if (doc.student_gender === "Female") {
-          return [__("Female"), "female-indicator", "student_gender,=,Female"]; // Light pink
-      } else if (doc.student_gender === "Male") {
-          return [__("Male"), "male-indicator", "student_gender,=,Male"]; // Light blue
-      } else {
-          return [__("Other"), "other-indicator", "student_gender,=,Other"]; // Light brown (tan)
-      }
+    console.log("Processing Student:", doc.name, "with student_gender:", doc.student_gender); // Debugging log
+    if (doc.student_gender === "Female") {
+        console.log("Applying female-indicator to", doc.name); // Debugging log
+        return [__("Female"), "female-indicator", "student_gender,=,Female"]; // Light pink
+    } else if (doc.student_gender === "Male") {
+        console.log("Applying male-indicator to", doc.name); // Debugging log
+        return [__("Male"), "male-indicator", "student_gender,=,Male"]; // Light blue
+    } else {
+        return [__("Other"), "other-indicator", "student_gender,=,Other"]; // Light brown (tan)
+    }
   }
 };
