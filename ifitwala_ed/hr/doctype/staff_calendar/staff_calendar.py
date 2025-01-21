0aedd3c7cfd3e9f43929a8cd0db9f858fd1b638a
Francois de Ryckel <f.deryckel@gmail.com>
2025-01-21 23:15:36 +0700

    remove student portal in modules

--- ifitwala_ed/hr/doctype/staff_calendar/staff_calendar.py
+++ ifitwala_ed/hr/doctype/staff_calendar/staff_calendar.py
@@ -6,7 +6,7 @@ import json
 from datetime import date
 
 from frappe import _
-from frappe.utils import getdate, today, formatdate, cint, date_diff
+from frappe.utils import getdate, formatdate, date_diff
 from frappe.model.document import Document
 
 
