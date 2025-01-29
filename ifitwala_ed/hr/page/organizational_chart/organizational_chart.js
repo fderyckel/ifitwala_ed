b903e3192cbea4cbad0198a412df69fc9f93402a
Francois de Ryckel <f.deryckel@gmail.com>
2025-01-29 12:36:43 +0700

    fdsa

--- ifitwala_ed/hr/page/organizational_chart/organizational_chart.js
+++ ifitwala_ed/hr/page/organizational_chart/organizational_chart.js
@@ -10,14 +10,37 @@ frappe.pages["organizational-chart"].on_page_load = function (wrapper) {
 			let organizational_chart;
 			let method = "ifitwala_ed.hr.page.organizational_chart.organizational_chart.get_children";
 
-			if (frappe.is_mobile()) {
-				organizational_chart = new ifitwala_ed.HierarchyChartMobile("Employee", wrapper, method);
-			} else {
-				organizational_chart = new ifitwala_ed.HierarchyChart("Employee", wrapper, method);
-			}
+			console.log("Fetching organization chart data...");
 
-			frappe.breadcrumbs.add("HR");
-			organizational_chart.show();
-		});
-	});
+      // Fetch and log the data
+      frappe.call({
+        method: method,
+        args: {},
+        callback: function (response) {
+          console.log("Organization chart data:", response.message);
+
+          if (!response.message || response.message.length === 0) {
+            console.warn("No data received for the organization chart.");
+            return;
+          }
+
+          if (frappe.is_mobile()) {
+            organizational_chart = new ifitwala_ed.HierarchyChartMobile("Employee", wrapper, method);
+          } else {
+            organizational_chart = new ifitwala_ed.HierarchyChart("Employee", wrapper, method);
+          }
+
+          frappe.breadcrumbs.add("HR");
+
+          // Slight delay to ensure DOM is ready before rendering
+          setTimeout(() => {
+            organizational_chart.show();
+          }, 500);
+        },
+        error: function (err) {
+          console.error("Error fetching organization chart data:", err);
+        },
+      });
+    });
+  });			
 };
