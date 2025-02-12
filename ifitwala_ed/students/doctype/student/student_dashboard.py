90c8e1d8569b78cbc0cf8ee29d2142267fc83c12
Francois de Ryckel <f.deryckel@gmail.com>
2025-02-13 01:23:19 +0700

    adding student dashboard

---
+++ ifitwala_ed/students/doctype/student/student_dashboard.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2025, François de Ryckel and contributors
+# For license information, please see license.txt
+
+from frappe import _
+
+def get_data(): 
+  return {
+    fieldname: "student",
+    transactions: [
+      {
+        label: _("Academic"),
+        items: ["Program Enrollment", "Course Enrollment", "Student Group"]
+      }, 
+      {label: _("Communication"),
+       items: ["Student Log"]
+       }, 
+      {
+        label: _("Health"),
+        items: ["Student Patient", "Student Patient Visit"]
+      }
+    ]
+  }
