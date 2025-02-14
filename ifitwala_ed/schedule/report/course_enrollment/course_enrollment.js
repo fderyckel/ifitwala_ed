c65a5dab756de99191051b526fbf2c3a2fe99165
Francois de Ryckel <f.deryckel@gmail.com>
2025-02-14 14:10:34 +0700

    report script for enrollment

--- ifitwala_ed/schedule/report/course_enrollment/course_enrollment.js
+++ ifitwala_ed/schedule/report/course_enrollment/course_enrollment.js
@@ -1,8 +1,46 @@
 // Copyright (c) 2025, François de Ryckel and contributors
 // For license information, please see license.txt
 
-frappe.query_reports["Course Enrollment"] = {
+frappe.query_reports["Program Enrollment Report"] = {
 	"filters": [
-
-	]
+		{
+			"fieldname": "report_type",
+			"label": "Report Type",
+			"fieldtype": "Select", 
+			"options": "\nProgram\nCourse",
+			"default": "Program"
+		},
+		{
+			"fieldname": "school",
+			"label": "School",
+			"fieldtype": "Link",
+			"options": "School",
+			"reqd": 1
+		},
+		{
+			"fieldname": "program",
+			"label": "Program",
+			"fieldtype": "Link",
+			"options": "Program",
+			"reqd": 1
+		},
+		{
+			"fieldname": "term",
+			"label": "Term",
+			"fieldtype": "Link",
+			"options": "Term",
+			"reqd": 0,
+			"depends_on": "eval:doc.report_type=='Program'"
+		},
+		{
+			"fieldname": "academic_year",
+			"label": "Academic Year",
+			"fieldtype": "Link",
+			"options": "Academic Year",
+			"reqd": 0,
+			"depends_on": "eval:doc.report_type=='Course'"
+		}
+	],
+	"chart_type": "bar",
+	"default_columns": 2
 };
