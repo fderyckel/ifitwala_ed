a1f05a5d3ab1a1d30d6d89fb43b53025dcb60bb6
Francois de Ryckel <f.deryckel@gmail.com>
2025-02-14 14:19:15 +0700

    redid report

--- ifitwala_ed/schedule/report/enrollment/enrollment.js
+++ ifitwala_ed/schedule/report/enrollment/enrollment.js
@@ -3,6 +3,44 @@
 
 frappe.query_reports["Enrollment"] = {
 	"filters": [
-
-	]
+		{
+			"fieldname": "report_type",
+			"label": "Report Type",
+			"fieldtype": "Select", 
+			"options": "\nProgram\nCourse",
+			"default": "Program"
+		},
+		{
+			"fieldname": "school",
+			"label": "School",
+			"fieldtype": "Link",
+			"options": "School",
+			"reqd": 1
+		},
+		{
+			"fieldname": "program",
+			"label": "Program",
+			"fieldtype": "Link",
+			"options": "Program",
+			"reqd": 1
+		},
+		{
+			"fieldname": "term",
+			"label": "Term",
+			"fieldtype": "Link",
+			"options": "Term",
+			"reqd": 0,
+			"depends_on": "eval:doc.report_type=='Program'"
+		},
+		{
+			"fieldname": "academic_year",
+			"label": "Academic Year",
+			"fieldtype": "Link",
+			"options": "Academic Year",
+			"reqd": 0,
+			"depends_on": "eval:doc.report_type=='Course'"
+		}
+	],
+	"chart_type": "bar",
+	"default_columns": 2
 };
--- ifitwala_ed/schedule/report/enrollment/enrollment.py
+++ ifitwala_ed/schedule/report/enrollment/enrollment.py
@@ -1,9 +1,103 @@
 # Copyright (c) 2025, François de Ryckel and contributors
 # For license information, please see license.txt
 
-# import frappe
-
+import frappe
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    if not filters:
+        filters = {}
+    
+    report_type = filters.get("report_type", "Program")
+    
+    if report_type == "Course":
+        columns = get_course_columns(filters)
+        data = get_course_data(filters)
+        chart = get_course_chart_data(data)
+    else:
+        columns = get_program_columns(filters)
+        data = get_program_data(filters)
+        chart = get_program_chart_data(data)
+    
+    return columns, data, None, chart
+
+def get_program_columns(filters):
+    return [
+        {"label": "Term", "fieldname": "term", "fieldtype": "Data", "width": 200},
+        {"label": "Enrollment Count", "fieldname": "enrollment_count", "fieldtype": "Int", "width": 150},
+    ]
+
+def get_program_data(filters):
+    # Validate required filters for Program view
+    if not filters.get("school") or not filters.get("program"):
+        frappe.throw("Please set School and Program filters.")
+    
+    term_condition = ""
+    if filters.get("term"):
+        term_condition = "AND term = %(term)s"
+    
+    sql = f"""
+    SELECT
+        IFNULL(term, 'Not Specified') as term,
+        COUNT(name) as enrollment_count
+    FROM `tabProgram Enrollment`
+    WHERE status = 1
+      AND school = %(school)s
+      AND program = %(program)s
+      {term_condition}
+    GROUP BY term
+    ORDER BY enrollment_count DESC
+    """
+    return frappe.db.sql(sql, filters, as_dict=True)
+
+def get_program_chart_data(data):
+    return {
+        "data": {
+            "labels": [row.term for row in data],
+            "datasets": [
+                {"name": "Enrollments", "values": [row.enrollment_count for row in data]}
+            ]
+        },
+        "type": "bar",
+        "fieldtype": "Data",
+        "colors": ["#7cd6fd"]
+    }
+
+def get_course_columns(filters):
+    return [
+        {"label": "Course", "fieldname": "course", "fieldtype": "Data", "width": 200},
+        {"label": "Enrollment Count", "fieldname": "enrollment_count", "fieldtype": "Int", "width": 150},
+    ]
+
+def get_course_data(filters):
+    # Validate required filters for Course view
+    if not filters.get("school") or not filters.get("program") or not filters.get("academic_year"):
+        frappe.throw("Please set School, Program, and Academic Year filters.")
+    
+    sql = """
+    SELECT
+        ce.course as course,
+        COUNT(ce.name) as enrollment_count
+    FROM `tabCourse Enrollment` ce
+    JOIN `tabProgram Enrollment` pe ON ce.program_enrollment = pe.name
+    WHERE ce.current = 1
+      AND ce.academic_year = %(academic_year)s
+      AND pe.school = %(school)s
+      AND pe.program = %(program)s
+    GROUP BY ce.course
+    ORDER BY enrollment_count DESC
+    """
+    return frappe.db.sql(sql, filters, as_dict=True)
+
+def get_course_chart_data(data):
+    return {
+        "data": {
+            "labels": [row.course for row in data],
+            "datasets": [
+                {"name": "Enrollments", "values": [row.enrollment_count for row in data]}
+            ]
+        },
+        "type": "bar",
+        "fieldtype": "Data",
+        "colors": ["#7cd6fd"]
+    }
+
