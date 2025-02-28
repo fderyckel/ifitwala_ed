2781edca29446b5cf1adffb19313eb21d9dcd2ad
Francois de Ryckel <f.deryckel@gmail.com>
2025-02-28 19:43:37 +0800

    forgot to upload the libraries

--- ifitwala_ed/students/doctype/student/student.py
+++ ifitwala_ed/students/doctype/student/student.py
@@ -2,6 +2,10 @@
 # For license information, please see license.txt
 
 import frappe
+import os
+import random
+import string
+from frappe.utils import get_files_path
 from frappe import _
 from frappe.utils import getdate, today, get_link_to_form, validate_email_address
 from frappe.model.document import Document
