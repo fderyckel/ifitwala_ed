03b45f53521c95cfa757dcdb34e8968f32d76710
Francois de Ryckel <f.deryckel@gmail.com>
2025-03-20 11:54:13 +0700

    do not save

--- ifitwala_ed/schedule/doctype/school_schedule/school_schedule.py
+++ ifitwala_ed/schedule/doctype/school_schedule/school_schedule.py
@@ -63,8 +63,8 @@ class SchoolSchedule(Document):
             schedule_day.num_blocks = 0  # Default value, user must update later
 
         # Save the document to persist the changes
-        #self.flags.ignore_validate = True # Skip validation to allow saving
-        self.save()
+        self.flags.ignore_validate = True # Skip validation to allow saving
+        #self.save()
 
         frappe.msgprint(f"{self.rotation_days} Rotation Days have been generated.")
 
