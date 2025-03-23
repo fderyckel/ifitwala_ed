60be20a951f9bb468904ce669b223e782417fd4a
Francois de Ryckel <f.deryckel@gmail.com>
2025-03-23 10:07:23 +0700

    Add event retrieval and utility functions for academic scheduling

--- ifitwala_ed/schedule/doctype/schedule/schedule.py
+++ ifitwala_ed/schedule/doctype/schedule/schedule.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2025, François de Ryckel and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe.query_builder import DocType
+from ifitwala_ed.schedule.schedule_utils import get_rotation_dates, current_academic_year
+from frappe import _
 
 class Schedule(Document):
 	
@@ -26,5 +28,82 @@ class Schedule(Document):
 
 	@staticmethod
 	def get_stats(args):
-		pass
+		pass 
+  
+	@frappe.whitelist() 
+	def get_events(self, start, end, filters=None):
+		user = frappe.session.user
+		roles = frappe.get_roles(user)
+		academic_year = filters.get("academic_year") if filters else current_academic_year()
+
+		instructor = None
+
+		if "Academic Admin" in roles:
+			instructor = filters.get("instructor")
+			if not instructor:
+				frappe.throw(_("Please select an instructor."))
+		elif "Instructor" in roles:
+			instructor = frappe.db.get_value("Instructor", {"user_id": user}, "name")
+			if not instructor:
+				frappe.throw(_("Instructor profile not found."))
+		else:
+			frappe.throw(_("Insufficient permissions."))
+
+		schedule_entries = []
+
+		StudentGroup = DocType("Student Group")
+		StudentGroupInstructor = DocType("Student Group Instructor")
+		StudentGroupSchedule = DocType("Student Group Schedule") 
+		
+		groups = ( 
+			frappe.qb.from_(StudentGroup) 
+			.inner_join(StudentGroupInstructor).on(StudentGroupInstructor.parent == StudentGroup.name)
+      .select(
+        StudentGroup.name, StudentGroup.student_group_name,
+        StudentGroup.course, StudentGroup.school
+      )
+      .where(
+        (StudentGroupInstructor.instructor == instructor) &
+        (StudentGroup.academic_year == academic_year) &
+        (StudentGroup.status == 'Active')
+      )
+      ).run(as_dict=True) 
+		
+		for group in groups: 
+			school_schedule_name = frappe.db.get_value("School Schedule", {"school": group.school}, "name")
+			if not school_schedule_name:
+				continue
+
+			school_schedule = frappe.get_cached_doc("School Schedule", school_schedule_name)
+			rotation_dates = get_rotation_dates(school_schedule_name, academic_year, school_schedule.include_holidays_in_rotation)
+
+			schedules = (
+				frappe.qb.from_(StudentGroupSchedule)
+				.select(
+					StudentGroupSchedule.rotation_day,
+					StudentGroupSchedule.block_number,
+					StudentGroupSchedule.from_time,
+					StudentGroupSchedule.to_time,
+					StudentGroupSchedule.room
+				)
+				.where(StudentGroupSchedule.parent == group.name)
+			).run(as_dict=True)
+			
+			rotation_map = {}
+			for rd in rotation_dates:
+				rotation_map.setdefault(rd["rotation_day"], []).append(rd["date"])
+
+			for s in schedules:
+				dates = rotation_map.get(s.rotation_day, [])
+				for date in dates:
+					schedule_entries.append({
+						"name": f"{group.student_group_name}-{s.rotation_day}-{s.block_number}",
+						"title": f"{group.course} ({group.student_group_name})",
+						"start": frappe.utils.get_datetime(f"{date} {s.from_time}"),
+						"end": frappe.utils.get_datetime(f"{date} {s.to_time}"),
+						"allDay": 0,
+						"color": "#74b9ff"
+					})
+
+		return schedule_entries
 
