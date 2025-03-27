1ba4a3b4bf5ab1f33c819582d60c9b384d3cb478
Francois de Ryckel <f.deryckel@gmail.com>
2025-03-27 09:29:44 +0700

    Refactor student image handling: improve real-time updates and streamline renaming logic

--- ifitwala_ed/students/doctype/student/student.js
+++ ifitwala_ed/students/doctype/student/student.js
@@ -3,36 +3,30 @@
 
 frappe.ui.form.on('Student', {
 	setup: function(frm) {
-      frm.set_query('student', 'siblings', function(doc) {
-        return {
-          'filters': {
-            'name': ['!=', doc.name]
-          }
-        };
-      });
-    },
+    frm.set_query('student', 'siblings', function(doc) {
+      return {
+        'filters': {'name': ['!=', doc.name]}
+      };
+    });
+  },
 
-    refresh: function(frm) {
-  		frappe.dynamic_link = {doc: frm.doc, fieldname: 'name', doctype: 'Student'};
 
-  		if (!frm.is_new()) {
-  			frappe.contacts.render_address_and_contact(frm);
-  		} else {
-  			frappe.contacts.clear_address_and_contact(frm);
-  		}
+  refresh: function(frm) {
+  	frappe.dynamic_link = {doc: frm.doc, fieldname: 'name', doctype: 'Student'};
+  		
+    if (!frm.is_new()) {
+  		frappe.contacts.render_address_and_contact(frm);
+  	} else {
+  		frappe.contacts.clear_address_and_contact(frm);
+  	}
 
-      frappe.realtime.on('student_image_updated', (data) => {
-        if (data.student === frm.doc.name) {
-            frm.reload_doc();
-            frappe.show_alert({message:__("Student image updated."), indicator:'green'});
-        }
-      });
-
-      // Sync pending_student_image safely
-      if (frm.doc.pending_student_image && frm.doc.student_image !== frm.doc.pending_student_image) {
-          frm.set_value('student_image', frm.doc.pending_student_image);
-          frm.set_value('pending_student_image', '');
-          frm.save();
-      }     
-    }
+    frappe.realtime.on('student_image_updated', (data) => {
+      if (data.student === frm.doc.name) {
+        frm.reload_doc().then(() => {
+          frm.set_value('student_image', data.file_url);
+          frappe.show_alert({message: __("Student image updated. Please save the document."), indicator: 'green'});
+         });
+      }
+    });  
+  }
 });
--- ifitwala_ed/utilities/student_utils.py
+++ ifitwala_ed/utilities/student_utils.py
@@ -9,15 +9,10 @@ from frappe.utils import get_files_path
 from frappe import _
 
 def rename_and_move_student_image(student_docname, current_image_url):
-    student = frappe.get_doc("Student", student_docname)
     current_file_name = os.path.basename(current_image_url)
 
-    # Check if already renamed
-    if (
-        current_file_name.startswith(student_docname + "_")
-        and len(current_file_name.split("_")[1].split(".")[0]) == 6
-        and current_file_name.split(".")[1].lower() in ["jpg", "jpeg", "png", "gif"]
-    ):
+    if (current_file_name.startswith(student_docname + "_") and
+        len(current_file_name.split("_")[1].split(".")[0]) == 6):
         return
 
     file_extension = os.path.splitext(current_file_name)[1]
@@ -25,9 +20,7 @@ def rename_and_move_student_image(student_docname, current_image_url):
     expected_file_name = f"{student_docname}_{random_suffix}{file_extension}"
 
     student_folder_fm_path = "Home/student"
-    expected_file_path = os.path.join(student_folder_fm_path, expected_file_name)
 
-    # Ensure "student" folder exists
     if not frappe.db.exists("File", {"file_name": "student", "folder": "Home"}):
         student_folder = frappe.get_doc({
             "doctype": "File",
@@ -37,7 +30,6 @@ def rename_and_move_student_image(student_docname, current_image_url):
         })
         student_folder.insert(ignore_permissions=True)
 
-    # Original file doc
     file_name = frappe.db.get_value("File",
         {
             "file_url": current_image_url,
@@ -49,8 +41,7 @@ def rename_and_move_student_image(student_docname, current_image_url):
 
     if not file_name:
         frappe.log_error(
-            title=_("Missing File Doc"),
-            message=_("No File doc found for {0}, attached_to=Student {1}").format(current_image_url, student_docname)
+            _("No File doc found for {0}, attached_to=Student {1}").format(current_image_url, student_docname)
         )
         return
 
@@ -59,28 +50,19 @@ def rename_and_move_student_image(student_docname, current_image_url):
     old_file_path = os.path.join(get_files_path(), file_doc.file_name)
     new_file_path = os.path.join(get_files_path(), "student", expected_file_name)
 
-    try:
-        if os.path.exists(old_file_path):
-            os.rename(old_file_path, new_file_path)
-        else:
-            frappe.throw(_("Original file not found: {0}").format(old_file_path))
+    if os.path.exists(old_file_path):
+        os.rename(old_file_path, new_file_path)
+    else:
+        frappe.log_error(_("Original file not found: {0}").format(old_file_path))
+        return
 
-        file_doc.file_name = expected_file_name
-        file_doc.file_url = f"/files/student/{expected_file_name}"
-        file_doc.folder = "Home/student"
-        file_doc.is_private = 0
-        file_doc.save(ignore_permissions=True)
+    file_doc.file_name = expected_file_name
+    file_doc.file_url = f"/files/student/{expected_file_name}"
+    file_doc.folder = "Home/student"
+    file_doc.is_private = 0
+    file_doc.save(ignore_permissions=True)
 
-        # Update student_image in Student doc safely via db.set_value
-        frappe.db.set_value("Student", student_docname, "pending_student_image",
-            file_doc.file_url,
-            update_modified=False  # Prevents changing modified timestamp
-        )
-
-        frappe.publish_realtime('student_image_updated', {'student': student_docname})
-
-    except Exception as e:
-        frappe.log_error(
-            title=_("Student Image Rename Error"),
-            message=f"Error renaming student image for {student_docname}: {e}"
-        )
+    frappe.publish_realtime('student_image_updated', {
+        'student': student_docname,
+        'file_url': file_doc.file_url
+    })
