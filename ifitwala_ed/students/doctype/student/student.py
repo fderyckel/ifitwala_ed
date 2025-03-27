9b1dde68723b4b2a11b1112f8fa30b82a4cfa332
Francois de Ryckel <f.deryckel@gmail.com>
2025-03-27 08:22:56 +0700

    Implement asynchronous image renaming and moving for student profiles; add utility function for file handling.

--- ifitwala_ed/students/doctype/student/student.py
+++ ifitwala_ed/students/doctype/student/student.py
@@ -2,15 +2,12 @@
 # For license information, please see license.txt
 
 import frappe
-import os
-import random
-import string
-from frappe.utils import get_files_path
 from frappe import _
 from frappe.utils import getdate, today, get_link_to_form, validate_email_address
 from frappe.model.document import Document
 from frappe.desk.form.linked_with import get_linked_doctypes
 from frappe.contacts.address_and_contact import load_address_and_contact
+from ifitwala_ed.student.student_utils import rename_and_move_student_image
 
 
 class Student(Document):
@@ -61,6 +58,15 @@ class Student(Document):
 		self.update_student_user()
 		self.update_student_patient()
 		self.update_links()
+		
+		if self.student_image: 
+			frappe.enqueue(
+				rename_and_move_student_image, 
+				student_docname=self.name,
+				current_image_url=self.student_image,
+				queue='default', 
+				timeout=300
+      )	
 
 	# create student as website user
 	def create_student_user(self):
@@ -96,6 +102,9 @@ class Student(Document):
 			student_patient.save()
 			frappe.msgprint(_("Student Patient {0} linked to this student has been created").format(self.student_full_name))
 
+
+###### Update methods ######
+	# update the student contact card if info in student changes
 	def update_links(self): 
 		# Fetch the contact name linked to the user (student_email)
 		contact_name = frappe.db.get_value("Contact", {"user": self.student_email}, "name")  
@@ -112,7 +121,6 @@ class Student(Document):
 		else:
 			frappe.msgprint(_("No Contact found for Student Email: {0}. Ensure a Contact is created.").format(self.student_email)) 
 
-	
 	# will update user main info if the student info change  
 	def update_student_user(self): 
 		user = frappe.get_doc("User", self.student_email) 
@@ -124,6 +132,7 @@ class Student(Document):
 			user.gender = self.student_gender 
 		user.save()
 
+	# disactivate the student patient if the student is disactivated
 	def update_student_patient(self):
 		patient = frappe.db.get_value("Student Patient", {"student":self.name}, "name")
 		if self.enabled == 0:
@@ -131,7 +140,7 @@ class Student(Document):
 		else:
 			frappe.db.set_value("Student Patient", patient, "status", "Active")
 
-
+####### From schedule module #######
 	def enroll_in_course(self, course_name, program_enrollment, enrollment_date):
 		try:
 			enrollment = frappe.get_doc({
---
+++ ifitwala_ed/students/student_utils.py
@@ -0,0 +1,89 @@
+# Copyright (c) 2025, François de Ryckel and contributors
+# For license information, please see license.txt
+
+import frappe
+import os
+import random
+import string
+from frappe.utils import get_files_path
+from frappe import _
+
+def rename_and_move_student_image(student_docname, current_image_url):
+    student = frappe.get_doc("Student", student_docname)
+    current_file_name = os.path.basename(current_image_url)
+
+    # Check if already renamed
+    if (
+        current_file_name.startswith(student_docname + "_")
+        and len(current_file_name.split("_")[1].split(".")[0]) == 6
+        and current_file_name.split(".")[1].lower() in ["jpg", "jpeg", "png", "gif"]
+    ):
+        return
+
+    file_extension = os.path.splitext(current_file_name)[1]
+    random_suffix = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+    expected_file_name = f"{student_docname}_{random_suffix}{file_extension}"
+
+    student_folder_fm_path = "Home/student"
+    expected_file_path = os.path.join(student_folder_fm_path, expected_file_name)
+
+    # Ensure "student" folder exists
+    if not frappe.db.exists("File", {"file_name": "student", "folder": "Home"}):
+        student_folder = frappe.get_doc({
+            "doctype": "File",
+            "file_name": "student",
+            "is_folder": 1,
+            "folder": "Home"
+        })
+        student_folder.insert(ignore_permissions=True)
+
+    # Original file doc
+    file_name = frappe.db.get_value("File",
+        {
+            "file_url": current_image_url,
+            "attached_to_doctype": "Student",
+            "attached_to_name": student_docname
+        },
+        "name"
+    )
+
+    if not file_name:
+        frappe.log_error(
+            title=_("Missing File Doc"),
+            message=_("No File doc found for {0}, attached_to=Student {1}").format(current_image_url, student_docname)
+        )
+        return
+
+    file_doc = frappe.get_doc("File", file_name)
+
+    old_file_path = os.path.join(get_files_path(), file_doc.file_name)
+    new_file_path = os.path.join(get_files_path(), "student", expected_file_name)
+
+    try:
+        if os.path.exists(old_file_path):
+            os.rename(old_file_path, new_file_path)
+        else:
+            frappe.throw(_("Original file not found: {0}").format(old_file_path))
+
+        file_doc.file_name = expected_file_name
+        file_doc.file_url = f"/files/student/{expected_file_name}"
+        file_doc.folder = "Home/student"
+        file_doc.is_private = 0
+        file_doc.save(ignore_permissions=True)
+
+        # Update student_image in Student doc safely via db.set_value
+        frappe.db.set_value(
+            "Student",
+            student_docname,
+            "student_image",
+            file_doc.file_url,
+            update_modified=False  # Prevents changing modified timestamp
+        )
+
+        frappe.publish_realtime('student_image_updated', {'student': student_docname})
+
+    except Exception as e:
+        frappe.log_error(
+            title=_("Student Image Rename Error"),
+            message=f"Error renaming student image for {student_docname}: {e}"
+        )
