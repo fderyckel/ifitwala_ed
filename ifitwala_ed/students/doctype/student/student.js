cc8f6ba28831bd8694e421c6e86e3a996153f3a6
Francois de Ryckel <f.deryckel@gmail.com>
2025-03-27 09:38:54 +0700

    Enhance student image update handling: implement debounce save and improve real-time notifications

--- ifitwala_ed/students/doctype/student/student.js
+++ ifitwala_ed/students/doctype/student/student.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Student', {
-	setup: function(frm) {
+  setup: function(frm) {
     frm.set_query('student', 'siblings', function(doc) {
       return {
         'filters': {'name': ['!=', doc.name]}
@@ -10,23 +10,31 @@ frappe.ui.form.on('Student', {
     });
   },
 
-
   refresh: function(frm) {
-  	frappe.dynamic_link = {doc: frm.doc, fieldname: 'name', doctype: 'Student'};
-  		
+    frappe.dynamic_link = {doc: frm.doc, fieldname: 'name', doctype: 'Student'};
+
     if (!frm.is_new()) {
-  		frappe.contacts.render_address_and_contact(frm);
-  	} else {
-  		frappe.contacts.clear_address_and_contact(frm);
-  	}
+      frappe.contacts.render_address_and_contact(frm);
+    } else {
+      frappe.contacts.clear_address_and_contact(frm);
+    }
 
     frappe.realtime.on('student_image_updated', (data) => {
       if (data.student === frm.doc.name) {
         frm.reload_doc().then(() => {
-          frm.set_value('student_image', data.file_url);
-          frappe.show_alert({message: __("Student image updated. Please save the document."), indicator: 'green'});
-         });
+          if (frm.doc.student_image !== data.file_url) {
+            frm.set_value('student_image', data.file_url).then(() => {
+              // explicit debounce save (ensures reload completes fully)
+              setTimeout(() => {
+                frm.save().then(() => {
+                  frappe.show_alert({message: __("Student image updated successfully."), indicator: 'green'});
+                });
+              }, 500); // half-second ensures safe debounce
+            });
+          }
+        });
       }
-    });  
+    });
   }
 });
+
