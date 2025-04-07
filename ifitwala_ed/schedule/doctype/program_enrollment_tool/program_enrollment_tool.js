a8d7114ddb94f3de5a56355996ace2184971526e
Francois de Ryckel <f.deryckel@gmail.com>
2025-04-07 21:30:02 +0700

    Enhance Program Enrollment Tool: Refactor student fetching logic and add dynamic academic year queries based on selected program.

--- ifitwala_ed/schedule/doctype/program_enrollment_tool/program_enrollment_tool.js
+++ ifitwala_ed/schedule/doctype/program_enrollment_tool/program_enrollment_tool.js
@@ -2,12 +2,14 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Program Enrollment Tool', {
-
+	
 	refresh: function(frm) {
 		frm.disable_save();
-		frm.fields_dict.enroll_students.$input.addClass(' btn btn-primary'); 
+		frm.fields_dict.enroll_students.$input.addClass('btn btn-primary');
+		frm.add_fetch('student', 'student_full_name', 'student_name');
 
-    frm.add_fetch('student', 'student_full_name', 'student_name');
+		// Setup dynamic field toggle
+		toggle_filter_fields(frm);
 
 		frappe.realtime.on("program_enrollment_tool", data => {
 			frappe.hide_msgprint(true);
@@ -15,7 +17,50 @@ frappe.ui.form.on('Program Enrollment Tool', {
 		});
 	},
 
-	// logic for the "get students" button.  Calling the get_students function in .py file.
+	get_students_from: function(frm) {
+		toggle_filter_fields(frm);
+	},
+
+	program: function(frm) {
+		frm.set_value("academic_year", null);
+
+		if (frm.doc.program) {
+			frappe.db.get_value("Program", frm.doc.program, "school")
+				.then(({ message }) => {
+					if (message && message.school) {
+						frm.set_query("academic_year", function () {
+							return {
+								query: "ifitwala_ed.schedule.doctype.program_enrollment.program_enrollment.get_academic_years",
+								filters: {
+									school: message.school
+								}
+							};
+						});
+					}
+				});
+		}
+	},
+
+	new_program: function(frm) {
+		frm.set_value("new_academic_year", null);
+
+		if (frm.doc.new_program) {
+			frappe.db.get_value("Program", frm.doc.new_program, "school")
+				.then(({ message }) => {
+					if (message && message.school) {
+						frm.set_query("new_academic_year", function () {
+							return {
+								query: "ifitwala_ed.schedule.doctype.program_enrollment.program_enrollment.get_academic_years",
+								filters: {
+									school: message.school
+								}
+							};
+						});
+					}
+				});
+		}
+	},
+
 	get_students: function(frm) {
 		if (!frm.doc.get_students_from) {
 			frappe.msgprint(__('Please select "Get Students From" before fetching students.'));
@@ -33,27 +78,43 @@ frappe.ui.form.on('Program Enrollment Tool', {
 			frappe.msgprint(__('Please select a Student Cohort.'));
 			return;
 		}
-		
+
 		frm.set_value("students", []);
 		frappe.call({
 			doc: frm.doc,
 			method: "get_students",
 			callback: function(r) {
-				if(r.message) {
+				if (r.message) {
 					frm.set_value("students", r.message);
 				}
 			}
 		});
 	},
 
-	// logic for the "enroll student" button.
 	enroll_students: function(frm) {
 		frappe.call({
-			doc:frm.doc,
+			doc: frm.doc,
 			method: "enroll_students",
-			callback: function(r) {
+			callback: function() {
 				frm.set_value("students", []);
 			}
 		});
 	}
 });
+
+function toggle_filter_fields(frm) {
+	const source = frm.doc.get_students_from;
+
+	frm.toggle_display('program', false);
+	frm.toggle_display('academic_year', false);
+	frm.toggle_display('student_cohort', false);
+
+	if (source === "Program Enrollment") {
+		frm.toggle_display('program', true);
+		frm.toggle_display('academic_year', true);
+		frm.toggle_display('student_cohort', true); // optional
+	}
+	else if (source === "Cohort") {
+		frm.toggle_display('student_cohort', true);
+	}
+}
