57b8dc578e6ea918f5b86f9c77d2cb098b379b66
Francois de Ryckel <f.deryckel@gmail.com>
2025-04-23 22:43:01 +0700

    tired!

--- ifitwala_ed/admission/web_form/registration_of_interest/registration_of_interest.py
+++ ifitwala_ed/admission/web_form/registration_of_interest/registration_of_interest.py
@@ -14,5 +14,5 @@ def get_valid_academic_years():
         "Academic Year",
         filters={"year_end_date": [">=", today]},
         fields=["name"],
-        order_by="start_date asc"
+        order_by="year_start_date asc"
     )
