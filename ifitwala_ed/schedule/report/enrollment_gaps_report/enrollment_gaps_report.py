b05c47779a0b26d218ef35a7c3bd29878b45d457
Francois de Ryckel <f.deryckel@gmail.com>
2025-04-23 10:04:33 +0700

    fds

--- ifitwala_ed/schedule/report/enrollment_gaps_report/enrollment_gaps_report.py
+++ ifitwala_ed/schedule/report/enrollment_gaps_report/enrollment_gaps_report.py
@@ -1,8 +1,5 @@
 # Copyright (c) 2025, François de Ryckel and contributors
 # License: MIT. See license.txt
 
-# Query Report logic handled via SQL file.
-# This file exists only for metadata compliance.
-
 def execute(filters=None):
     return []
