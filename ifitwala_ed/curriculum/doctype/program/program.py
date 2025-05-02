dcfa55c1e0569074c9f5b91a6c30ecc6556a87f5
Francois de Ryckel <f.deryckel@gmail.com>
2025-05-02 07:26:17 +0700

    dsc

--- ifitwala_ed/curriculum/doctype/program/program.py
+++ ifitwala_ed/curriculum/doctype/program/program.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (c) 2024, Francois de Ryckel  and contributors
 # For license information, please see license.txt
 
@@ -8,59 +7,56 @@ from frappe.website.website_generator import WebsiteGenerator
 from frappe.utils.nestedset import NestedSet
 from frappe import _dict
 
-class Program(NestedSet):
+class Program(WebsiteGenerator):
+    # which field becomes the slug in the URL
+    website_generator = "program_slug"
 
-	def validate(self):
-		self.validate_duplicate_course()
+    # generator settings: route prefix, template, publish flag, title field
+    website = _dict(
+        route="program",
+        template="templates/generators/program.html",
+        condition_field="is_published",
+        page_title_field="program_name",
+    )
 
-	def validate_duplicate_course(self):
-		found = []
-		for course in self.courses:
-			doc = frappe.get_doc("Course", course.course)
-			if doc.status != "Active":
-				frappe.throw(_("Course {0} has been discontinued. It cannot be part of this program or set up this course as active.").format(course.course))
+    def validate(self):
+        # your duplicate‐course guard
+        self.validate_duplicate_course()
+        # ensure route is recalculated on every save
+        self.set_route()
 
-			if course.course in found:
-				frappe.throw(_("Course {0} is entered twice. Please remove one of them.").format(course.course))
-			else:
-				found.append(course.course)
-	
+    def validate_duplicate_course(self):
+        seen = set()
+        for row in self.courses:
+            if row.course in seen:
+                frappe.throw(_("Course {0} entered twice").format(row.course))
+            seen.add(row.course)
 
-	class Program(WebsiteGenerator):
-		website_generator = "program_slug" 
-	
-		website = _dict(
-			template = "templates/generators/program.html",
-			condition_field = "is_published",
-			page_title_field = "program_name",
-  	)	
-		def get_context(self, context):
-			# disable caching so editors see changes immediately
-			context.no_cache = True
+    def get_context(self, context):
+        # stop caching so edits appear immediately
+        context.no_cache = True
 
-			# basics
-			context.title = self.program_name
-			context.program = self  # so your template can do {{ program.program_overview }}
+        # page basics
+        context.title = self.program_name
+        context.program = self
 
-			# breadcrumbs
-			crumbs = []
-			# top‐level “Programs” landing page
-			crumbs.append({"name": "/programs", "title": "Programs"})
+        # breadcrumbs back to /programs
+        crumbs = [{"name": "/programs", "title": "Programs"}]
+        parent = self.parent_program
+        while parent:
+            p = frappe.get_doc("Program", parent)
+            crumbs.append({
+                "name": f"/program/{p.program_slug}",
+                "title": p.program_name
+            })
+            parent = p.parent_program
+        context.parents = crumbs
 
-			# any parent programs
-			parent = self.parent_program
-			while parent:
-				p = frappe.get_doc("Program", parent)
-				crumbs.append({"name": f"/program/{p.program_slug}", "title": p.program_name})
-				parent = p.parent_program
+        # if this node is a group, list its children
+        if self.is_group:
+            context.children = [
+                frappe.get_doc("Program", child.name)
+                for child in self.get_children()
+            ]
 
-			context.parents = crumbs
-
-			# if this is a group, list its children
-			context.children = []
-			if self.is_group:
-				for child in self.get_children():
-					doc = frappe.get_doc("Program", child.name)
-					context.children.append(doc)
-
-			return context
+        return context
