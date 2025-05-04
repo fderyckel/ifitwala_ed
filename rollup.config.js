819838227d8b0dc337aca4e1a407405b0079ddf1
Francois de Ryckel <f.deryckel@gmail.com>
2025-05-04 11:30:33 +0700

    bjkl

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,23 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: {
-    website: 'ifitwala_ed/public/website/website.js',
-    school: 'ifitwala_ed/public/website/school.js',
+export default [
+  {
+    input: 'ifitwala_ed/public/website/website.js',
+    output: {
+      file: 'ifitwala_ed/public/dist/website.bundle.js',
+      format: 'iife',
+      sourcemap: true,
+    },
+    plugins: [resolve(), commonjs(), terser()],
   },
-  output: {
-    dir: 'ifitwala_ed/public/dist',
-    format: 'iife',
-    entryFileNames: '[name].bundle.js',
-    sourcemap: true,
+  {
+    input: 'ifitwala_ed/public/website/school.js',
+    output: {
+      file: 'ifitwala_ed/public/dist/school.bundle.js',
+      format: 'iife',
+      sourcemap: true,
+    },
+    plugins: [resolve(), commonjs(), terser()],
   },
-  plugins: [
-    resolve(),
-    commonjs(),
-    terser(),
-  ],
-};
+];
