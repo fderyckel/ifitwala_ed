d9e5bfe0729085c0dc44c2f638bf50fe4aa653ec
Francois de Ryckel <f.deryckel@gmail.com>
2025-05-06 13:27:42 +0700

    refactor tree view in Employee

--- ifitwala_ed/hr/doctype/employee/employee_tree.js
+++ ifitwala_ed/hr/doctype/employee/employee_tree.js
@@ -4,45 +4,51 @@
 frappe.treeview_settings["Employee"] = {
 	get_tree_nodes: "ifitwala_ed.hr.doctype.employee.employee.get_children",
 	filters: [
-		{
-			fieldname: "organization",
-			fieldtype: "Select",
-			options: ["All Organizations"].concat(ifitwala_ed.utils.get_tree_options("organization")),
-			label: __("Organization"),
-			default: "All Organizations" //ifitwala_ed.utils.get_tree_default("organization"),
-		},
+			{
+					fieldname: "organization",
+					fieldtype: "Select",
+					options: ["All Organizations"].concat(
+							ifitwala_ed.utils.get_tree_options("organization")
+					),
+					label: __("Organization"),
+					default: "All Organizations",
+			},
 	],
-	breadcrumb: "Hr",
+	breadcrumb: "HR",
 	disable_add_node: true,
 	get_tree_root: false,
 	toolbar: [
-		{ toggle_btn: true },
-		{
-			label: __("Edit"),
-			condition: function (node) {
-				return !node.is_root;
+			{ toggle_btn: true },
+			{
+					label: __("Edit"),
+					condition: function (node) {
+							return !node.is_root;
+					},
+					click: function (node) {
+							frappe.set_route("Form", "Employee", node.data.value);
+					},
 			},
-			click: function (node) {
-				frappe.set_route("Form", "Employee", node.data.value);
-			},
-		},
 	],
 	menu_items: [
-		{
-			label: __("New Employee"),
-			action: function () {
-				frappe.new_doc("Employee", true);
+			{
+					label: __("New Employee"),
+					action: function () {
+							frappe.new_doc("Employee", true);
+					},
+					condition:
+							'frappe.boot.user.can_create.indexOf("Employee") !== -1',
 			},
-			condition: 'frappe.boot.user.can_create.indexOf("Employee") !== -1',
-		},
 	],
 
-	onload(tree) {
-		// Build the tree
-		tree.make_tree();
+	onload(treeview) {
+			// Build the tree structure
+			treeview.make_tree();
+	},
 
-		// Expand all nodes once rendered
-		tree.expand_all();
+	post_render(treeview) {
+			// Once rendered, expand all nodes in the actual Tree instance
+			if (treeview.tree && typeof treeview.tree.open_all === "function") {
+					treeview.tree.open_all();
+			}
 	}
-
 };
