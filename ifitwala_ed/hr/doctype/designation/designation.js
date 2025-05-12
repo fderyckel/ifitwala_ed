16b7f4a5f8e52f8dcd09561976697acbe5cbc540
Francois de Ryckel <f.deryckel@gmail.com>
2025-05-12 11:14:58 +0700

    client-side filtering logic for designation

--- ifitwala_ed/hr/doctype/designation/designation.js
+++ ifitwala_ed/hr/doctype/designation/designation.js
@@ -1,8 +1,33 @@
-// Copyright (c) 2024, fdR and contributors
+// Copyright (c) 2025, fdR and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Designation", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Designation", {
+  organization: function(frm) {
+      if (frm.doc.organization && frm.doc.organization !== "All Organizations") {
+          // Apply the dynamic filter for the selected organization
+          frm.fields_dict["school"].get_query = function() {
+              return {
+                  filters: {
+                      organization: frm.doc.organization
+                  }
+              };
+          };
+      } else {
+          // Clear the school if the organization is empty or set to "All Organizations"
+          frm.set_value("school", "");
+      }
+  },
+  
+  refresh: function(frm) {
+      // Apply the filter on form load as well
+      if (frm.doc.organization && frm.doc.organization !== "All Organizations") {
+          frm.fields_dict["school"].get_query = function() {
+              return {
+                  filters: {
+                      organization: frm.doc.organization
+                  }
+              };
+          };
+      }
+  }
+});
