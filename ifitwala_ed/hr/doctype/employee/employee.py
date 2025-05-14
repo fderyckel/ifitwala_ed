2539ee34c351494ea6baf822c590559507c611b2
Francois de Ryckel <f.deryckel@gmail.com>
2025-05-14 19:15:03 +0700

    trying again sorting

--- ifitwala_ed/hr/doctype/employee/employee.py
+++ ifitwala_ed/hr/doctype/employee/employee.py
@@ -299,9 +299,10 @@ class Employee(NestedSet):
 
 		# Combine sorted current and past roles
 		sorted_history = current_roles + past_roles
+		self.history = sorted_history
 
 		# Reassign the sorted list to the child table
-		for idx, row in enumerate(sorted_history, start=1):
+		for idx, row in enumerate(self.history, start=1):
 			row.idx = idx
 
 	# call on validate.  Check that if there is already a user, a few more checks to do.
