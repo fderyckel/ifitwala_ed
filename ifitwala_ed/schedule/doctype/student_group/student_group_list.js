273dac10918fa4fa801e071809bc19238be9fe42
Francois de Ryckel <f.deryckel@gmail.com>
2025-05-25 14:30:09 +0700

    update type of group in list view

--- ifitwala_ed/schedule/doctype/student_group/student_group_list.js
+++ ifitwala_ed/schedule/doctype/student_group/student_group_list.js
@@ -2,15 +2,28 @@
 // For license information, please see license.txt
 
 frappe.listview_settings["Student Group"] = {
-  filters: [["status", "=", "Active"]],
-  hide_name_column: true,
-  get_indicator: function (doc) {
-    var indicator = [
-      __(doc.status),
-      frappe.utils.guess_colour(doc.status),
-      "status,=," + doc.status,
-    ];
-    indicator[1] = { Active: "green", Retired: "darkgrey" }[doc.status];
-    return indicator;
-  },
+	filters: [["status", "=", "Active"]],
+
+	// Left-most indicator
+	get_indicator(doc) {
+		const color_map = {
+			Active: "green",
+			Retired: "darkgrey",
+		};
+		return [__(doc.status), color_map[doc.status] || "blue", `status,=,${doc.status}`];
+	},
+
+	// Column-specific badges
+	formatters: {
+		group_based_on(value) {
+			const color_map = {
+				Cohort: "primary",
+				Course: "purple",
+				Activity: "orange",
+				Other: "secondary",
+			};
+			const color = color_map[value] || "dark";
+			return `<span class="badge bg-${color} text-white">${__(value)}</span>`;
+		},
+	},
 };
