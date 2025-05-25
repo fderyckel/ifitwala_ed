b0a77792c8e8b63c097887cc6af58a811af62f5c
Francois de Ryckel <f.deryckel@gmail.com>
2025-05-25 16:03:55 +0700

    refactor employee

--- ifitwala_ed/schedule/doctype/instructor/instructor.py
+++ ifitwala_ed/schedule/doctype/instructor/instructor.py
@@ -7,21 +7,26 @@ from frappe.model.document import Document
 from frappe.utils.user import add_role
 
 class Instructor(Document):
-	def __setup__(self):
-		self.onload()
+
+	def autoname(self):
+			full_name = frappe.db.get_value("Employee", self.employee, "employee_full_name")
+			if not full_name:
+					frappe.throw(_("Cannot set Instructor name without employee full name."))
+			self.name = full_name
 
 	def onload(self):
 		self.load_groups()
 
 	def validate(self):
-		self.instructor_log = []
 		self.validate_duplicate_employee()
+		user_id = frappe.db.get_value("Employee", self.employee, "user_id")
+		if not user_id:
+			frappe.throw(_("Linked Employee must have a User ID."))
+
+		self.user_id = user_id
 
 	def after_insert(self):
-		from frappe.utils.user import add_role
 		add_role(self.user_id, "Instructor")
-		if self.status == "Active":
-			add_role(self.user_id, "Newsletter Manager")
 
 	def on_update(self):
 		if self.status == "Inactive":
@@ -37,8 +42,37 @@ class Instructor(Document):
 
 	def load_groups(self):
 		self.instructor_log = []
-		groups = frappe.get_all("Student Group Instructor", filters = {"instructor":self.name}, fields = ["parent", "designation"])
-		for group in groups:
-			yo = frappe.get_doc("Student Group", group.parent)
-			self.append("instructor_log", {"academic_year":yo.academic_year, "academic":yo.academic,
-						"designation":group.designation, "student_group":yo.name, "course":yo.course})
+
+		# Step 1: Get Student Group Instructor links
+		group_links = frappe.db.get_values(
+			"Student Group Instructor",
+			filters={"instructor": self.name},
+			fieldname=["parent", "designation"],
+			as_dict=True
+		)
+
+		if not group_links:
+			return
+
+		# Step 2: Batch fetch Student Group info
+		student_group_names = [g["parent"] for g in group_links]
+		group_records = frappe.db.get_values(
+			"Student Group",
+			filters={"name": ["in", student_group_names]},
+			fieldname=["name", "academic_year", "academic", "course"],
+			as_dict=True
+		)
+
+		group_lookup = {g["name"]: g for g in group_records}
+
+		# Step 3: Append to child table
+		for link in group_links:
+			group_doc = group_lookup.get(link["parent"])
+			if group_doc:
+				self.append("instructor_log", {
+					"academic_year": group_doc["academic_year"],
+					"academic": group_doc["academic"],
+					"designation": link["designation"],
+					"student_group": group_doc["name"],
+					"course": group_doc["course"]
+				})
