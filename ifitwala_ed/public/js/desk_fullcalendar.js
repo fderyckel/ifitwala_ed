fdef183411ae476b83601ec53dae9f28cf9a2585
Francois de Ryckel <f.deryckel@gmail.com>
2025-05-30 10:12:30 +0700

    issue with js and css

--- ifitwala_ed/public/js/desk_fullcalendar.js
+++ ifitwala_ed/public/js/desk_fullcalendar.js
@@ -8,11 +8,6 @@ import dayGridPlugin     from "@fullcalendar/daygrid";
 import timeGridPlugin    from "@fullcalendar/timegrid";
 import listPlugin        from "@fullcalendar/list";
 
-/* CSS for all three view plugins */
-import "@fullcalendar/core/index.css";
-import "@fullcalendar/daygrid/index.css";
-import "@fullcalendar/timegrid/index.css";
-import "@fullcalendar/list/index.css";
 
 /* Make it behave like the old “index.global” build */
 window.FullCalendar = { Calendar, dayGridPlugin, timeGridPlugin, listPlugin };
