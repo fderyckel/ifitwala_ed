b671a2b1ded655bd509db4b6921e4c0bd074cb40
Francois de Ryckel <f.deryckel@gmail.com>
2025-06-01 20:43:38 +0700

    das

--- ifitwala_ed/public/js/ifitwala_ed.bundle.js
+++ ifitwala_ed/public/js/ifitwala_ed.bundle.js
@@ -5,5 +5,9 @@ import "./desk_fullcalendar"
 import "../css/student_group_cards.css";
 
 import { renderStudentCard } from "./student_group_cards.js";
-frappe.provide("ifitwala_ed.helpers");
-frappe.ifitwala_ed.helpers.renderStudentCard = renderStudentCard;
+
+// -- Make sure the namespace exists even if Frappe JS isn't loaded yet
+const root = (window.frappe = window.frappe || {});      // guarantee global frappe
+root.ifitwala_ed = root.ifitwala_ed || {};
+root.ifitwala_ed.helpers = root.ifitwala_ed.helpers || {};
+root.ifitwala_ed.helpers.renderStudentCard = renderStudentCard;
