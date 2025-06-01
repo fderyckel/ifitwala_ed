a89b757946ad674e62b936fd4cc95f09bf5de484
Francois de Ryckel <f.deryckel@gmail.com>
2025-06-02 02:06:19 +0700

    fuuutytyfytf

--- ifitwala_ed/public/js/ifitwala_ed.bundle.js
+++ ifitwala_ed/public/js/ifitwala_ed.bundle.js
@@ -1,10 +1,7 @@
-
 import "./utils";
 import "./queries";
 import "./desk_fullcalendar" 
-
 import "../css/student_group_cards.css";  
-
 
 import { renderStudentCard } from "./student_group_cards.js";
 
