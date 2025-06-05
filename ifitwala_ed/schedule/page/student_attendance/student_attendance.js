0a5313fea9dd3d31d46e630c662698eaf1e55897
Francois de Ryckel <f.deryckel@gmail.com>
2025-06-05 09:49:56 +0700

    renaming student attendance tool (to avoid clash with the doctype wich is in the student module)

--- ifitwala_ed/schedule/page/student_attendance/student_attendance.js
+++
@@ -1,7 +0,0 @@
-frappe.pages['student-attendance-e'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Student Attendance Tool',
-		single_column: true
-	});
-}
