38912136c212b73c8a642ba7d7af90f099d0672a
Francois de Ryckel <f.deryckel@gmail.com>
2025-06-06 09:19:16 +0700

    argh!!!

--- ifitwala_ed/public/js/desk_fullcalendar.js
+++ ifitwala_ed/public/js/desk_fullcalendar.js
@@ -2,18 +2,12 @@
  * (one tiny bundle, shared by every desk page that needs it)
  */
 
-
-
 import { Calendar } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
 
-/* CSS for all three view plugins */
-import "@fullcalendar/core/index.css";
-import "@fullcalendar/daygrid/index.css";
-import "@fullcalendar/timegrid/index.css";
-import "@fullcalendar/list/index.css";
+
 
 /* Make it behave like the old “index.global” build */
 window.FullCalendar = { Calendar, dayGridPlugin, timeGridPlugin, listPlugin };
