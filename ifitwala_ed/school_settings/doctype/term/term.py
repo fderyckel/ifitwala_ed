d58786168069224dde57638119004db7ca4ddb44
Francois de Ryckel <f.deryckel@gmail.com>
2025-06-09 08:54:16 +0700

    didn't import nowdate

--- ifitwala_ed/school_settings/doctype/term/term.py
+++ ifitwala_ed/school_settings/doctype/term/term.py
@@ -3,7 +3,7 @@
 
 import frappe
 from frappe import _
-from frappe.utils import getdate, cstr, get_link_to_form
+from frappe.utils import getdate, nowdate, cstr, get_link_to_form
 from frappe.model.document import Document
 from frappe.utils.nestedset import get_ancestors_of
 from ifitwala_ed.utilities.school_tree import ParentRuleViolation
