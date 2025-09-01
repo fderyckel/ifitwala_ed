a73844b85a2396e717fe80fc697bace75bcbbddc
Francois <f.deryckel@gmail.com>
2025-09-01 12:43:38 +0000

    new doctype for referral case

---
+++ ifitwala_ed/students/report/case_entries_activity_log/case_entries_activity_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, François de Ryckel and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Case Entries Activity Log"] = {
+	"filters": [
+
+	]
+};
