13eda504a6667234595e45787a6eda0884015754
Francois de Ryckel <f.deryckel@gmail.com>
2025-09-17 21:15:10 +0700

    issue with import css

--- ifitwala_ed/ui-spa/src/main.ts
+++ ifitwala_ed/ui-spa/src/main.ts
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'frappe-ui/dist/style.css'
+import { FrappeUI } from 'frappe-ui'
+
+// Tailwind entry (keep this file with @tailwind directives)
 import './style.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+	.use(FrappeUI)
+	.use(router)
+	.mount('#app')
+
