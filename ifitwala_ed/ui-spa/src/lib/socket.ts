c1eb1c55b6ff0aaf30f3814f58435650aa57edcb
Francois de Ryckel <f.deryckel@gmail.com>
2025-09-18 07:47:57 +0700

    fds

--- ifitwala_ed/ui-spa/src/lib/socket.ts
+++ ifitwala_ed/ui-spa/src/lib/socket.ts
@@ -2,8 +2,8 @@
 import { io } from 'socket.io-client'
 
 // Force protocol-relative URL so HTTP stays HTTP in tests
-export const socket = io(`//${location.host}`, {
+export const socket = io(`//${location.hostname}:9000`, {
   path: '/socket.io',
   transports: ['websocket', 'polling'],
   withCredentials: true
-})
+});
