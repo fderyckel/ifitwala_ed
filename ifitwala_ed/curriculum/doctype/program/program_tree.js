0c1337ddb46247d6addfd347424eb49a9938a76b
Francois de Ryckel <f.deryckel@gmail.com>
2025-10-03 11:47:55 +0700

    changing place for the styling files the refactor program trees

--- ifitwala_ed/curriculum/doctype/program/program_tree.js
+++ ifitwala_ed/curriculum/doctype/program/program_tree.js
@@ -1,43 +1,23 @@
 // Copyright (c) 2025, François de Ryckel and contributors
 // For license information, please see license.txt
 
+// ifitwala_ed/curriculum/doctype/program/program_tree.js
+
 frappe.treeview_settings["Program"] = {
 	breadcrumb: "Curriculum",
 	title: __("Program Tree"),
 	root_label: "All Programs",
-	get_tree_root: false, // let frappe load based on is_group + parent_program
+	get_tree_root: false, // NestedSet + parent_program drive the tree
 	get_tree_nodes: "frappe.desk.treeview.get_children",
 	add_tree_node: "frappe.desk.treeview.add_node",
-	filters: [
-		{
-			fieldname: "school",
-			fieldtype: "Link",
-			options: "School",
-			label: __("School"),
-			get_query: function () {
-				return {
-					filters: {
-						name: ["in", frappe.boot.user.defaults.school || []]
-					}
-				};
-			}
-		}
-	],
+
+	// Program is global (no School filter)
 	fields: [
-		{
-			fieldtype: "Data",
-			fieldname: "program_name",
-			label: __("Program Name"),
-			reqd: 1
-		},
-		{
-			fieldtype: "Check",
-			fieldname: "is_group",
-			label: __("Group Node")
-		},
+		{ fieldtype: "Data",  fieldname: "program_name", label: __("Program Name"), reqd: 1 },
+		{ fieldtype: "Check", fieldname: "is_group",     label: __("Group Node") }
 	],
 	ignore_fields: ["parent_program"],
-	onload: function(treeview) {
-		// optional callback
-	}
+
+	// onload not needed
 };
+
