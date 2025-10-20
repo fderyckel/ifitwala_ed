6f07af97b24fbdbe538738381aa7f4080038cb36
Francois de Ryckel <f.deryckel@gmail.com>
2025-10-20 15:20:51 +0700

    fdshiull

--- ifitwala_ed/setup/doctype/organization/organization.py
+++ ifitwala_ed/setup/doctype/organization/organization.py
@@ -6,29 +6,28 @@ from frappe import _
 from frappe.utils.nestedset import NestedSet
 from frappe.utils import cint
 
+
+
 class Organization(NestedSet):
 	def validate(self):
 		# Prevent changing parent of root node
 		if self.name == "All Organizations" and self.parent_organization:
 			frappe.throw(_("The root organization 'All Organizations' cannot have a parent."))
-
-		# Disallow adding a child under a non-group parent (ERPNext-like rule)
+		# Disallow adding a child under a non-group parent
 		if self.parent_organization:
 			parent_is_group = frappe.db.get_value("Organization", self.parent_organization, "is_group")
 			if not parent_is_group:
-				frappe.throw(
-					_("Parent Organization must be a Group. '{0}' is not a Group.")
-					.format(self.parent_organization)
-				)
+				frappe.throw(_("Parent Organization must be a Group. '{0}' is not a Group.")
+					.format(self.parent_organization))
 
 
 @frappe.whitelist()
-def get_children(doctype, parent=None, filters=None):
-	"""Return tree nodes: children of `parent`, or top-level groups if no parent."""
-	filters = filters or {}
+def get_children(doctype, parent=None, is_root=False, **kwargs):
+	"""Return tree nodes: children of `parent`, or top-level groups if root."""
+	filters = dict(kwargs.get("filters") or {})
 
-	# If no parent passed, show only groups (roots)
-	if not parent:
+	# Treat virtual root label as no parent
+	if is_root or not parent or parent == "All Organizations":
 		filters.update({"is_group": 1, "parent_organization": ["is", "not set"]})
 	else:
 		filters.update({"parent_organization": parent})
@@ -40,8 +39,9 @@ def get_children(doctype, parent=None, filters=None):
 			"organization_name as title",
 			"is_group as expandable",
 		],
+		# show in nested-set order
+		order_by="lft asc",
 		filters=filters,
-		order_by="organization_name asc",
 	)
 
 
@@ -61,18 +61,17 @@ def add_node(**kwargs):
 	"""
 	Create a new Organization node from the tree dialog.
 
-	Expected kwargs:
-	- organization_name (str)
-	- abbr (str)
+	Accepts both our prompt fields and the standard treeview args:
+	- organization_name (str, reqd)
+	- abbr (str, reqd)
 	- is_group (0/1)
-	- parent_organization (str | None | 'All Organizations')
+	- parent_organization OR parent (str | None | 'All Organizations')
 	"""
 	org_name = (kwargs.get("organization_name") or "").strip()
 	abbr = (kwargs.get("abbr") or "").strip()
 	is_group = cint(kwargs.get("is_group") or 0)
-	parent = kwargs.get("parent_organization")
 
-	# Root add: treat 'All Organizations' as no parent
+	parent = kwargs.get("parent_organization") or kwargs.get("parent")
 	if not parent or parent == "All Organizations":
 		parent = None
 
