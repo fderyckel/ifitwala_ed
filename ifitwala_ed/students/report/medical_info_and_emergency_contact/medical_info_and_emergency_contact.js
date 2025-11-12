770c6d69bd304b30db1abf8dafe877fb02cdfc53
Your Name <you@example.com>
2025-11-12 00:17:01 +0000

    new reports

---
+++ ifitwala_ed/students/report/medical_info_and_emergency_contact/medical_info_and_emergency_contact.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, François de Ryckel and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Medical Info And Emergency Contact"] = {
+	"filters": [
+
+	]
+};
---
+++ ifitwala_ed/students/report/medical_info_and_emergency_contact/medical_info_and_emergency_contact.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, François de Ryckel and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
