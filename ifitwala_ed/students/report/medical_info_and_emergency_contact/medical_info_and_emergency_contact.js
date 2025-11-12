37a096438ae37945aad2de71a7e21f529ae6692c
Francois de Ryckel <f.deryckel@gmail.com>
2025-11-12 07:54:47 +0700

    feat: Enhance Medical Info and Emergency Contact report with dynamic filters and detailed layout

--- ifitwala_ed/students/report/medical_info_and_emergency_contact/medical_info_and_emergency_contact.js
+++ ifitwala_ed/students/report/medical_info_and_emergency_contact/medical_info_and_emergency_contact.js
@@ -2,7 +2,58 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Medical Info And Emergency Contact"] = {
-	"filters": [
+	filters: [
+		{
+			fieldname: "school",
+			label: __("School"),
+			fieldtype: "Link",
+			options: "School"
+		},
+		{
+			fieldname: "program",
+			label: __("Program"),
+			fieldtype: "Link",
+			options: "Program"
+		},
+		{
+			fieldname: "student_group",
+			label: __("Student Group"),
+			fieldtype: "Link",
+			options: "Student Group",
+			reqd: 1
+		}
+	],
 
-	]
+	onload(report) {
+		const school_filter = report.get_filter("school");
+		const student_group_filter = report.get_filter("student_group");
+
+		if (student_group_filter) {
+			student_group_filter.get_query = () => ({
+				query: "ifitwala_ed.utilities.link_queries.student_group_link_query",
+				filters: {
+					school: frappe.query_report.get_filter_value("school"),
+					program: frappe.query_report.get_filter_value("program")
+				}
+			});
+		}
+
+		if (!school_filter) return;
+
+		frappe.call({
+			method: "ifitwala_ed.utilities.school_tree.get_user_default_school",
+			callback: (r) => {
+				const user_school = (r && r.message) || "";
+
+				school_filter.get_query = () => ({
+					query: "ifitwala_ed.utilities.school_tree.get_school_descendants",
+					filters: { root: user_school || frappe.query_report.get_filter_value("school") }
+				});
+
+				if (!frappe.query_report.get_filter_value("school") && user_school) {
+					frappe.query_report.set_filter_value("school", user_school);
+				}
+			}
+		});
+	}
 };
