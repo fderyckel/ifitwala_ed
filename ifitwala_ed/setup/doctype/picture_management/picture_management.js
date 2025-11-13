2da21d99e963e094f9b4dc88132260e7e94711b1
Your Name <you@example.com>
2025-11-13 01:07:43 +0000

    ds

---
+++ ifitwala_ed/setup/doctype/picture_management/picture_management.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, François de Ryckel and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Picture Management", {
+// 	refresh(frm) {
+
+// 	},
+// });
