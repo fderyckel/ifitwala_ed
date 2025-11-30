b577aacba19267fc205431c01d4fc09b3a197c93
Your Name <you@example.com>
2025-11-30 05:41:51 +0000

    dsadas

---
+++ ifitwala_ed/setup/doctype/org_communication/test_org_communication.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, François de Ryckel and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestOrgCommunication(FrappeTestCase):
+	pass
