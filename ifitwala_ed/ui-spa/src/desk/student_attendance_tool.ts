93ce1ba4b6d74ae80352845dff2cbee255acea33
Francois de Ryckel <f.deryckel@gmail.com>
2025-11-30 21:45:39 +0700

    hgl

--- ifitwala_ed/ui-spa/src/desk/student_attendance_tool.ts
+++ ifitwala_ed/ui-spa/src/desk/student_attendance_tool.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import { FrappeUI, setConfig, toast } from 'frappe-ui'
 import StudentAttendanceTool from '@/pages/staff/schedule/student-attendance-tool/StudentAttendanceTool.vue'
-import '@/style.css'
 import 'frappe-ui/style.css'
+import '@/style.css'
 import { setupFrappeUI } from '@/resources/frappe'
 
 // Ensure frappe-ui resources use the standard frappeRequest helper
