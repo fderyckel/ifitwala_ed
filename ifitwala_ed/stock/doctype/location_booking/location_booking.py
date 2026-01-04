292876bae030ada64130a3bb9a317b98b8fbeaf5
Your Name <you@example.com>
2026-01-04 08:37:15 +0000

    new location booking

--- ifitwala_ed/stock/doctype/location_booking/location_booking.py
+++ ifitwala_ed/stock/doctype/location_booking/location_booking.py
@@ -1,9 +1,9 @@
-# Copyright (c) 2025, François de Ryckel and contributors
+# Copyright (c) 2026, François de Ryckel and contributors
 # For license information, please see license.txt
 
 # import frappe
 from frappe.model.document import Document
 
 
-class EmployeeGroupTable(Document):
+class LocationBooking(Document):
 	pass
