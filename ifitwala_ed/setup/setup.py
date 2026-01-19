4b1e68460e02c6ff2494dadb6f12b9f1959210e1
Francois de Ryckel <f.deryckel@gmail.com>
2026-01-19 15:47:36 +0700

    remove transport coordinator no doctype for it yet.

--- ifitwala_ed/setup/setup.py
+++ ifitwala_ed/setup/setup.py
@@ -164,7 +164,6 @@ def add_other_records(country=None):
 		{"doctype": "Student Log Next Step", "next_step": "Refer to Learning Support", "associated_role": "Learning Support"},
 		{"doctype": "Student Log Next Step", "next_step": "Behaviour follow-up", "associated_role": "Pastoral Lead"},
 		{"doctype": "Student Log Next Step", "next_step": "IT / Device support", "associated_role": "Organization IT"},
-		{"doctype": "Student Log Next Step", "next_step": "Transport / Bus follow-up", "associated_role": "Transport Coordinator"},
 		{"doctype": "Student Log Next Step", "next_step": "Refer to Nurse / Health", "associated_role": "Nurse"},
 
 		# Program tree root (global)
