8a8533ed889128b072e21625cbe8e41fd9bdce4e
Your Name <you@example.com>
2026-02-11 01:58:27 +0000

    DSFCAS

---
+++ ifitwala_ed/school_site/doctype/website_seo_profile/test_website_seo_profile.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, François de Ryckel and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestWebsiteSEOProfile(FrappeTestCase):
+	pass
